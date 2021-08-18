4e84584cda435c57e646e9f2e25fc8855f2f09af
Huei <kk99887507@gmail.com>
2021-08-18 23:03:16 +0800

    forum listener & navController.navigate

--- app/src/main/java/idv/example/goodposture/user/forum/ForumAddFragment.java
+++ app/src/main/java/idv/example/goodposture/user/forum/ForumAddFragment.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import idv.example.goodposture.R;
 
 public class ForumAddFragment extends Fragment {
+    private ImageView iv_back2;
+    private ImageView iv_send;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,8 +35,30 @@ public class ForumAddFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         findViews(view);
+        handleback2();
+        handlesend();
     }
 
     private void findViews(View view) {
+        iv_back2 = view.findViewById(R.id.iv_back2);
+        iv_send = view.findViewById(R.id.iv_send);
+    }
+
+    private void handleback2() {
+        iv_back2.setOnClickListener( view ->{
+            // 取得NavController物件
+            NavController navController = Navigation.findNavController(view);
+            // 跳至頁面
+            navController.navigate(R.id.action_forumAddFragment_to_forumBrowseFragment2);
+        });
+    }
+
+    private void handlesend() {
+        iv_send.setOnClickListener( view ->{
+            // 取得NavController物件
+            NavController navController = Navigation.findNavController(view);
+            // 跳至頁面
+            navController.navigate(R.id.action_forumAddFragment_to_forumBrowseFragment2);
+        });
     }
 }
