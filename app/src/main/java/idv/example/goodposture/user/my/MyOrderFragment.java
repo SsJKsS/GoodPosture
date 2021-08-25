9609b89a20b60b2a8652ea309a9568515a061d99
VernaChan <hsuanhope@gmail.com>
2021-08-25 10:13:20 +0800

    just add myOrderFragment

---
+++ app/src/main/java/idv/example/goodposture/user/my/MyOrderFragment.java
@@ -0,0 +1,30 @@
+package idv.example.goodposture.user.my;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import idv.example.goodposture.R;
+
+public class MyOrderFragment extends Fragment {
+
+    public MyOrderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_order, container, false);
+    }
+}
