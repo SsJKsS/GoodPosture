220a30012b9c9f032463d6fbaf4a11a1cbaa297a
VernaChan <hsuanhope@gmail.com>
2021-08-31 22:02:48 +0800

    add Mypersonalinfo and MypersonalinfoEdit

--- app/src/main/java/idv/example/goodposture/user/my/MyPersonalInfoFragmentEdit.java
+++ app/src/main/java/idv/example/goodposture/user/my/MyPersonalInfoFragmentEdit.java
@@ -1,10 +1,15 @@
 package idv.example.goodposture.user.my;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,17 +17,9 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import idv.example.goodposture.R;
-import idv.example.goodposture.user.PreActivity;
 
-public class MyAccountFragment extends Fragment {
+public class MyPersonalInfoFragmentEdit extends Fragment {
     private AppCompatActivity activity;
     private Toolbar toolbar;
     private TextView tvAccountInfo, tvAccountPassword, tvAccountLogout;
@@ -37,7 +34,7 @@ public class MyAccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        return inflater.inflate(R.layout.fragment_my_personal_info_edit, container, false);
     }
 
     @Override
@@ -45,20 +42,20 @@ public class MyAccountFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         findViews(view);
         handleToolbar();
-        handleTv();
+        //handleTv();
     }
 
     private void findViews(View view) {
         toolbar = view.findViewById(R.id.tb_my);
-        tvAccountInfo = view.findViewById(R.id.tv_account_info);
-        tvAccountPassword = view.findViewById(R.id.tv_account_password);
-        tvAccountLogout = view.findViewById(R.id.tv_account_logout);
+//        tvAccountInfo = view.findViewById(R.id.tv_account_info);
+//        tvAccountPassword = view.findViewById(R.id.tv_account_password);
+//        tvAccountLogout = view.findViewById(R.id.tv_account_logout);
     }
 
     //顯示返回鍵
     private void handleToolbar() {
         activity.setSupportActionBar(toolbar);
-        toolbar.setTitle("我的帳號資料");
+        toolbar.setTitle("修改基本資料");
         ActionBar actionBar = activity.getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -69,7 +66,12 @@ public class MyAccountFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-        if(itemId == android.R.id.home){
+        if(itemId == R.id.my_personal_info_edit){
+            NavController navController = Navigation.findNavController(toolbar);
+            navController.navigate(R.id.action_fragmentShopping_to_shoppingCartFragment);
+            return true;
+        }
+        else if(itemId == android.R.id.home){
             Navigation.findNavController(toolbar).popBackStack();
         }else{
             return super.onOptionsItemSelected(item);
@@ -79,36 +81,15 @@ public class MyAccountFragment extends Fragment {
 
     private void handleTv() {
         tvAccountInfo.setOnClickListener(v -> {
-            NavController navController = Navigation.findNavController(tvAccountInfo);
-            navController.navigate(R.id.action_myAccountFragment_to_myPersonalInfoFragment);
+
         });
         tvAccountPassword.setOnClickListener( v -> {
             NavController navController = Navigation.findNavController(tvAccountPassword);
             navController.navigate(R.id.action_myAccountFragment_to_myResetPasswordFragment);
         });
         tvAccountLogout.setOnClickListener(v -> {
-            LogoutDialog();
+
         });
     }
 
-    private void LogoutDialog(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-        alertDialog.setMessage("是否確定登出");
-        alertDialog.setPositiveButton("否", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getActivity().getBaseContext(), "未登出", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        alertDialog.setNegativeButton("是", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(getContext(), PreActivity.class);
-                startActivity(intent);
-            }
-        });
-        alertDialog.setCancelable(false);   // disable click other area
-        alertDialog.show();
-    }
 }
